441dc115a6682dc72294bbe2d359e7b7ea1fee7d
danilo <danilo.mirkovic14@gmail.com>
2023-01-31 13:09:05 +0100

    adding a button with hooks for fetching data

--- fend/src/Components/activities.tsx
+++ fend/src/Components/activities.tsx
@@ -2,31 +2,53 @@ import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 export default function Activities() {
-  const [activityData, setActivityData] = useState("");
-  const [typeData, setTypeData] = useState("");
-  const [participantsData, setparticipantsData] = useState('');
-  const [priceData, setPriceData] = useState();
+    const [activityData, setActivityData] = useState("");
+    const [typeData, setTypeData] = useState("");
+    const [participantsData, setparticipantsData] = useState('');
+    const [priceData, setPriceData] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [err, setErr] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("https://www.boredapi.com/api/activity");
-      const json = await response.json();
-      setActivityData(json.activity);
-      setTypeData(json.type);
-      setparticipantsData(json.participants);
-      setPriceData(json.price);
-    }
-    fetchData();
-  }, []);  
-  // console.log(typeData);
-  
-  return (
 
-    <div>
-      <p>activity</p>{activityData}<br/>
-      <p>type of activity</p>{typeData}<br/>
-      <p>how many people</p>{participantsData}<br/>
-      <p>is it free</p>{priceData}
-    </div>
-  );
+    const handleClick = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await fetch('http://www.boredapi.com/api/activity/');
+
+            if (!response.ok) {
+                throw new Error(`Error! status: ${response.status}`);
+            }
+            const result = await response.json();
+
+            setActivityData(result.activity);
+            setparticipantsData(result.participants);
+            setPriceData(result.price);
+            setTypeData(result.type);
+        } catch (err) {
+            /* @ts-ignore */
+            setErr(err.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+
+        <div>
+            {err && <h2>{err}</h2>}
+
+            <button onClick={handleClick}>Fetch data</button>
+
+            {isLoading && <h2>Loading...</h2>}
+
+            <div>
+                <h2>{activityData}</h2>
+                <h2>{priceData}</h2>
+                <h2>{participantsData}</h2>
+                <h2>{typeData}</h2>
+            </div>
+
+        </div>
+    );
 }
